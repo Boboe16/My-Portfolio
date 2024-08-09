8cac2547264cf5a15c1e6ed3f2868e2f877fb734
Boboe16 <jerixmodz@gmail.com>
2024-08-09 18:13:23 +0800

    pee

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div id="contact-section" className="flex flex-col p-5 pt-24 mb-28">
-            <h1 className="font-abril text-5xl">LET'S</h1>
+            <h1 className="font-abril text-5xl">LET&apos;S</h1>
             <h1 className="font-abril text-5xl">CONNECT</h1>
             <h2 className="font-abril text-xl mb-4">I AM ALWAYS INTERESTED ABOUT</h2>
             <button className="font-lato text-primary shadow-md border rounded-md my-1 p-2 bg-tertiary hover:bg-primary hover:text-secondary border-black">FRONT END DEVELOPMENT</button>
